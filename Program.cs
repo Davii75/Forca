c6159b04020a3612167c6baf382546acf80264bc
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-24 10:17:29 -0300

    Programa que analisa se uma letra existe ou não em uma palavra específica fornecida pelo usuário

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string palavrasecreta;
+string letra;
+bool letraexiste;
+
+Console.WriteLine("--- FORCA ---");
+Console.WriteLine();
+Console.Write("Qual a palavra secreta? ");
+palavrasecreta = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Qual a letra? ");
+
+letra = Console.ReadLine()
+            .Trim()
+            .Substring(0, 1)
+            .ToLower();
+
+letraexiste = palavrasecreta
+                .ToLower()
+                .Contains(letra);
+
+Console.WriteLine();
+Console.WriteLine($"A letra {letra} existe na palavra secreta? ");
+Console.WriteLine($"{letraexiste}!");
